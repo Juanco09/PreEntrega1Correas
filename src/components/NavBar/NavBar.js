0e61ec857bb7fd8d0069ea057fc7f4c0df900e51
Juanco09 <correasjuan@gmail.com>
2023-02-21 22:17:37 -0300

    tercer commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./NavBar.css";
 import CartWidget from "./CartWidget.js";
+import logo from "../../assets/Logo-1.jpg";
 
 const NavBar = () => {
     return (
         <div className="NavBar">
+            <img className="logo" src={logo} />
             <h1>Luis Segundo Correas</h1>
             <nav>
                 <ul>
-                    <li><a href="">Malbec</a></li>
-                    <li><a href="">Chardonnay</a></li>
-                    <li><a href="">Torrontes</a></li>
-                    <li><a href="">Cabernet Sauvignon</a></li>
+                    <li><a href="">Tintos</a></li>
+                    <li><a href="">Blancos</a></li>
+                    <li><a href="">Rosados</a></li>
                 </ul>
             </nav>
             <CartWidget />
